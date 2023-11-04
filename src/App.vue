88d7b4e454c774276dde0ba0f861afdd842cfec6
tetsuyaEbina <tetsuya.ebina26@gmail.com>
2023-11-04 17:25:50 +0900

    feat_localStorageに値を保存する機能を実装

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
   import TheHeader from './components/TheHeader.vue';
+  import MainTodo from './components/MainTodo.vue';
   import TheFooter from './components/TheFooter.vue';
 </script>
 
@@ -7,7 +8,7 @@
 <template>
   <div class="wrap">
     <TheHeader />
-    <main class="main">メイン</main>
+    <main class="main"><MainTodo /></main>
     <TheFooter />
   </div>
 </template>
---
+++ src/components/MainTodo.vue
@@ -0,0 +1,52 @@
+<script setup>
+import { ref } from 'vue';
+const todoRef = ref('');
+const todoListRef = ref([]);
+
+const addTodo = () => {
+    // localStorageに.をつけて任意の変数名を設定することで、値を登録することができる
+    // TODO: DBと連携できるようにする
+    // IDを簡易的にm秒で管理
+    const id = new Date().getTime();
+    todoListRef.value.push({ id: id, task: todoRef.value })
+    localStorage.todoList = JSON.stringify(todoListRef.value)
+
+    // 登録後は、入力欄を空にする
+    todoRef.value = '';
+}
+</script>
+
+<template>
+    <div class="box_input">
+        <input 
+          type="text" 
+          class="todo_input" 
+          v-model="todoRef"
+          placeholder="TODOを入力" />
+        <button class="btn" v-on:click="addTodo">追加</button>
+    </div>
+</template>
+
+<style scoped>
+.box_input {
+    margin-top: 20px;
+}
+
+.todo_input {
+    width: 300px;
+    margin-right: 8px;
+    padding: 8px;
+    font-size: 18px;
+    border: 1px solid #aaa;
+    border-radius: 6px;
+}
+
+.btn {
+    padding: 8px;
+    background-color: #03a9f4;
+    border-radius: 6px;
+    color: #fff;
+    text-align: center;
+    font-size: 14px;
+}
+</style>
